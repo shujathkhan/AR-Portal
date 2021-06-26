1462b8e00ad7705b5bcb19d8df25239baa4307c5
shujathkhan <shujathlive@gmail.com>
2021-06-26 23:13:19 +0530

    Initial commit

---
+++ Assets/Scenes/PortalWindow/Scripts/ARController.cs
@@ -0,0 +1,160 @@
+﻿
+namespace GoogleARCore.Examples.HelloAR
+{
+    using System.Collections.Generic;
+    using GoogleARCore;
+    using GoogleARCore.Examples.Common;
+    using UnityEngine;
+    using UnityEngine.EventSystems;
+
+    #if UNITY_EDITOR
+        // Set up touch input propagation while using Instant Preview in the editor.
+        using Input = InstantPreviewInput;
+    #endif
+    public class ARController : MonoBehaviour
+    {
+        /// <summary>
+        /// The first-person camera being used to render the passthrough camera image (i.e. AR
+        /// background).
+        /// </summary>
+        public Camera FirstPersonCamera;
+
+        /// <summary>
+        /// The Portal door 
+        /// </summary>
+        public GameObject Portal;
+
+        /// <summary>
+        /// True if the app is in the process of quitting due to an ARCore connection error,
+        /// otherwise false.
+        /// </summary>
+        private bool m_IsQuitting = false;
+
+        /// <summary>
+        /// The Unity Awake() method.
+        /// </summary>
+        public void Awake()
+        {
+            // Enable ARCore to target 60fps camera capture frame rate on supported devices.
+            // Note, Application.targetFrameRate is ignored when QualitySettings.vSyncCount != 0.
+            Application.targetFrameRate = 60;
+        }
+
+        /// <summary>
+        /// The Unity Update() method.
+        /// </summary>
+        public void Update()
+        {
+            _UpdateApplicationLifecycle();
+
+            // If the player has not touched the screen, we are done with this update.
+            Touch touch;
+            if (Input.touchCount < 1 || (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+            {
+                return;
+            }
+
+            // Should not handle input if the player is pointing on UI.
+            if (EventSystem.current.IsPointerOverGameObject(touch.fingerId))
+            {
+                return;
+            }
+
+            // Raycast against the location the player touched to search for planes.
+            TrackableHit hit;
+            TrackableHitFlags raycastFilter = TrackableHitFlags.PlaneWithinPolygon |
+                TrackableHitFlags.FeaturePointWithSurfaceNormal;
+
+            if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+            {
+                Portal.SetActive(true);
+
+                Anchor anchor = hit.Trackable.CreateAnchor(hit.Pose);
+
+                Portal.transform.position = hit.Pose.position;
+                Portal.transform.rotation = hit.Pose.rotation;
+
+                Vector3 cameraPosition = FirstPersonCamera.transform.position;
+                cameraPosition.y = hit.Pose.position.y;
+
+                Portal.transform.LookAt(cameraPosition, Portal.transform.up);
+
+                Portal.transform.parent = anchor.transform;
+            }
+        }
+
+        /// <summary>
+        /// Check and update the application lifecycle.
+        /// </summary>
+        private void _UpdateApplicationLifecycle()
+        {
+            // Exit the app when the 'back' button is pressed.
+            if (Input.GetKey(KeyCode.Escape))
+            {
+                Application.Quit();
+            }
+
+            // Only allow the screen to sleep when not tracking.
+            if (Session.Status != SessionStatus.Tracking)
+            {
+                Screen.sleepTimeout = SleepTimeout.SystemSetting;
+            }
+            else
+            {
+                Screen.sleepTimeout = SleepTimeout.NeverSleep;
+            }
+
+            if (m_IsQuitting)
+            {
+                return;
+            }
+
+            // Quit if ARCore was unable to connect and give Unity some time for the toast to
+            // appear.
+            if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+            {
+                _ShowAndroidToastMessage("Camera permission is needed to run this application.");
+                m_IsQuitting = true;
+                Invoke("_DoQuit", 0.5f);
+            }
+            else if (Session.Status.IsError())
+            {
+                _ShowAndroidToastMessage(
+                    "ARCore encountered a problem connecting.  Please start the app again.");
+                m_IsQuitting = true;
+                Invoke("_DoQuit", 0.5f);
+            }
+        }
+
+        /// <summary>
+        /// Actually quit the application.
+        /// </summary>
+        private void _DoQuit()
+        {
+            Application.Quit();
+        }
+
+        /// <summary>
+        /// Show an Android toast message.
+        /// </summary>
+        /// <param name="message">Message string to show in the toast.</param>
+        private void _ShowAndroidToastMessage(string message)
+        {
+            AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
+            AndroidJavaObject unityActivity =
+                unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
+
+            if (unityActivity != null)
+            {
+                AndroidJavaClass toastClass = new AndroidJavaClass("android.widget.Toast");
+                unityActivity.Call("runOnUiThread", new AndroidJavaRunnable(() =>
+                {
+                    AndroidJavaObject toastObject =
+                        toastClass.CallStatic<AndroidJavaObject>(
+                            "makeText", unityActivity, message, 0);
+                    toastObject.Call("show");
+                }));
+            }
+        }
+    }
+}
